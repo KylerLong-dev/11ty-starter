0867a5feb114f870e84baf059937c8980fd99e6a
Kyler Long <kylerlong24@gmail.com>
2025-01-07 00:00:57 -0500

    Create scroll effect for sticky header that makes header full-width when scrolling down on page

--- src/javascript/nav.js
+++ src/javascript/nav.js
@@ -1,4 +1,22 @@
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => {
+
+document.addEventListener('click', () => {
+    const hamburger = document.querySelector('.hamburger');
     hamburger.classList.toggle('active');
 });
+
+//When scrolling down, nav becomes sticky and expands - need to smooth this out
+document.addEventListener("scroll", () => {
+    const pageHeader = document.querySelector('.page-header_container');
+    const headerNoPadding = document.querySelector('.page-header');
+
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        pageHeader.classList.add("page-header_full-width");
+        headerNoPadding.classList.add("page-header_no-padding");
+    } else {
+        pageHeader.classList.remove("page-header_full-width");
+        headerNoPadding.classList.remove("page-header_no-padding");
+    }
+});
+
+
+
