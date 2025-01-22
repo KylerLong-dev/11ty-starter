00dd0bf50ce2cfac6eaabf3a9c7f1802e5647567
Kyler Long <kylerlong24@gmail.com>
2025-01-21 22:47:58 -0500

    Add plugin for image optimization and edit portfolio section

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const eleventyPluginSharpImages = require("@codestitchofficial/eleventy-plugin-sharp-images");
 
 module.exports = function(eleventyConfig) {
 
@@ -7,9 +8,16 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin");
 
+    /*Fixes the way dates are displayed with blog entries*/
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+
+    /*Image Optimization Plugin*/
+    eleventyConfig.addPlugin(eleventyPluginSharpImages, {
+        urlPath: "/assets/images",
+        outputDir: "public/assets/images",
+    })
 
     return {
         dir: {
