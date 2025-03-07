2ebe385f42c05cd7a92de1131b017575f589a013
Kyler Long <kylerlong24@gmail.com>
2025-03-07 18:19:42 -0500

    Add _data folder with client.js file and global data to be used across project

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "Cypress Web Designs",
+    email: "cypressweb@gmail.com",
+    phoneForTel: "(555) 779-4407",
+    phoneFormatted: "(555) 779-4407",
+    address: {
+        lineOne: "First Address Line",
+        lineTwo: "Second Address Line",
+        city: "Orlando",
+        state: "FL",
+        zip: "32814",
+        country: "United States",
+        mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
+    },
+    socials: {
+        facebook: "https://www.facebook.com/",
+        instagram: "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    domain: "https://www.example.com",
+};
